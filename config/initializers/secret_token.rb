1ccc3bcbfd712212a7dda4d3bcb1ffc76a429a30
Anthony Erlinger <aerlinger@gmail.com>
2013-02-01 01:38:40 -0500

    Create app. Add Bootstrap, Rspec, and HighVoltage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homespree::Application.config.secret_token = '2052d768271b43b023aef2bcf109fc7b54e8f605fc6e9278ff397cede13e1273e18f314a815c5343805b7cdb30e40cdbf6a3bd8e0af9279d012a72cf567c3bb3'
