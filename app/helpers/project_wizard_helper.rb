02c3c3def0ac4db54c6ec25eb1d877d22fd37c5a
Anthony Erlinger <aerlinger@gmail.com>
2013-08-18 23:03:51 -0400

    Finish Interior Painting form

--- app/helpers/project_wizard_helper.rb
+++ app/helpers/project_wizard_helper.rb
@@ -3,10 +3,6 @@ module ProjectWizardHelper
     render "project_wizard/components/text_field", param: param, label: label
   end
 
-  def hs_select_field(param, label, tags)
-    render "project_wizard/components/select_field", param: param, label: label, tags: tags
-  end
-
   def hs_check_box_field(param, label, checked = false)
     render "project_wizard/components/check_box_field", param: param, label: label, tags: tags
   end
@@ -15,11 +11,19 @@ module ProjectWizardHelper
     render "project_wizard/components/number_field", param: param, label: label
   end
 
-  def hs_radio_collection_field(param, label, tags)
+  def hs_select_field(param, label, tags = {})
+    render "project_wizard/components/select_field", param: param, label: label, tags: tags
+  end
+
+  def hs_text_collection_field(param, label, tags = {})
+    render "project_wizard/components/text_collection_field", param: param, label: label, tags: tags
+  end
+
+  def hs_radio_collection_field(param, label, tags = {})
     render "project_wizard/components/check_box_field", param: param, label: label, tags: tags
   end
 
-  def hs_check_collection_field(param, label, tags)
+  def hs_check_collection_field(param, label, tags = {})
     render "project_wizard/components/check_box_field", param: param, label: label, tags: tags
   end
 end
